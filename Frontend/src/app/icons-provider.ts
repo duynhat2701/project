360b4526906ced67359e231bfcc2909f15b7a2c9
Duy Nhat <nhatnd2701@gmail.com>
2026-04-22 14:17:42 +0700

    thêm contact

--- Frontend/src/app/icons-provider.ts
+++ Frontend/src/app/icons-provider.ts
@@ -1,21 +1,25 @@
 import {
   AppstoreOutline,
+  BellOutline,
   MenuFoldOutline,
   MenuUnfoldOutline,
   FormOutline,
   DashboardOutline,
   LockOutline,
   MailOutline,
+  PhoneOutline,
   UserOutline,
 } from '@ant-design/icons-angular/icons';
 
 export const icons = [
   AppstoreOutline,
+  BellOutline,
   MenuFoldOutline,
   MenuUnfoldOutline,
   DashboardOutline,
   FormOutline,
   LockOutline,
   MailOutline,
+  PhoneOutline,
   UserOutline,
 ];
