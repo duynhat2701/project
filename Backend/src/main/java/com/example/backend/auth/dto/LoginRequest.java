a30018cb15d0821cbe63bc10bb41c2bcca5ba5ea
Duy Nhat <nhatnd2701@gmail.com>
2026-05-21 16:15:47 +0700

    thêm OTP

--- Backend/src/main/java/com/example/backend/auth/dto/LoginRequest.java
+++ Backend/src/main/java/com/example/backend/auth/dto/LoginRequest.java
@@ -2,7 +2,6 @@ package com.example.backend.auth.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,7 +11,6 @@ public class LoginRequest {
 
     @NotBlank(message = "Email is required")
     @Email(message = "Email is invalid")
-    @Pattern(regexp = "(?i)^[A-Z0-9._%+-]+@gmail\\.com$", message = "Email must end with @gmail.com")
     private String email;
 
     @NotBlank(message = "Password is required")
